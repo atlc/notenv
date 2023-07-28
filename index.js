6e784192d276f3e14d7d4e3772daa9fc6b164878
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2023-07-28 13:56:09 -0500

    Recreating dotenv with resiliency for the split characters as part of strings

---
+++ index.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const path = require("path");
+
+const envPath = path.join(__dirname, ".env");
+
+function config(path = envPath) {
+    const envString = fs.readFileSync(path).toString();
+    const lines = envString.split(/\n|\r/).filter((x) => x);
+
+    const temp = {};
+
+    lines.forEach((line) => {
+        const [key, ...value] = line.split("=");
+        if (!key || !value.length || !value[0]) return;
+        temp[key] = value.join("=");
+    });
+
+    Object.assign(process.env, { ...process.env, ...temp });
+}
+config();
+
+module.exports = {
+    config,
+};
